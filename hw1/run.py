fdb87bd611e7cf800021f5bea9695de04c7ed23f
Haohang Li <hli113@stevens.edu>
2023-10-05 12:25:15 -0400

    update nms

--- hw1/run.py
+++ hw1/run.py
@@ -56,7 +56,7 @@ def convolve2d(image: np.array, kernel: np.array) -> np.array:
     return output[half_k_h:-half_k_h, half_k_w:-half_k_w]
 
 
-# question 1
+# gaussian filter
 def gaussian_func(x: int, y: int, sigma: float, size: int) -> float:
     return (
         1
@@ -80,12 +80,189 @@ def gaussian_filter(
     return convolve2d(image, kernel), size
 
 
+# sobel operator
 def sobel_operator(image: np.array) -> Tuple[np.array, np.array]:
     # sobel operator
     sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
     # convolve
     return convolve2d(image, sobel_x), convolve2d(image, sobel_y)
+
+
+# non maximum suppression
+def map_to_closest_val(x: np.array, candidates: np.array) -> np.array:
+    dists = np.abs(x[:, np.newaxis] - candidates[:, np.newaxis])
+    return candidates[np.argmin(dists, axis=1)]
+
+
+def _direction_zero(
+    filtered_intensity: np.array, edge_map: np.array, i: int, j: int
+) -> np.array:
+    # at the left edge
+    if j < 1:
+        if filtered_intensity[i, j] >= filtered_intensity[i, j + 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i, j + 1] = 0
+    # at the right edge
+    elif j >= (edge_map.shape[1] - 1):
+        if filtered_intensity[i, j] >= filtered_intensity[i, j - 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i, j] = 1
+            edge_map[i, j - 1] = 0
+    # in the middle
+    elif filtered_intensity[i, j] >= max(
+        filtered_intensity[i, j - 1], filtered_intensity[i, j + 1]
+    ):
+        edge_map[i, j] = filtered_intensity[i, j]
+        edge_map[i, j - 1] = 0
+        edge_map[i, j + 1] = 0
+
+    return edge_map
+
+
+def _direction_45(
+    filtered_intensity: np.array, edge_map: np.array, i: int, j: int
+) -> np.array:
+    # 1. at the left edge
+    if j < 1:
+        # 1.1 at the bottom left corner
+        if i == (filtered_intensity.shape[0] - 1):
+            edge_map[i, j] = 0
+        elif filtered_intensity[i, j] >= filtered_intensity[i + 1, j + 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i + 1, j + 1] = 0
+    # 2. at the right edge
+    elif j >= (edge_map.shape[1] - 1):
+        # 2.1 at the top right corner
+        if i == 0:
+            edge_map[i, j] = 0
+        elif filtered_intensity[i, j] >= filtered_intensity[i - 1, j - 1]:
+            #edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i, j] = 1
+            edge_map[i - 1, j - 1] = 0
+    elif i < 1:
+        # 3.1 at the top left corner
+        if j == 0:
+            edge_map[i, j] = 0
+        # 3.2 at the top edge middle
+        elif filtered_intensity[i, j] >= filtered_intensity[i + 1, j + 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i + 1, j + 1] = 0
+    # 4. at the bottom edge
+    elif i >= (edge_map.shape[0] - 1):
+        # 4.1 at the bottom right corner
+        if j == (filtered_intensity.shape[1] - 1):
+            edge_map[i, j] = 0
+        # 4.2 at the bottom edge middle
+        elif filtered_intensity[i, j] >= filtered_intensity[i - 1, j - 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i - 1, j - 1] = 0
+    # 5. middle
+    elif filtered_intensity[i, j] >= max(
+        filtered_intensity[i - 1, j - 1], filtered_intensity[i + 1, j + 1]
+    ):
+        edge_map[i, j] = filtered_intensity[i, j]
+        edge_map[i - 1, j - 1] = 0
+        edge_map[i + 1, j + 1] = 0
+    return edge_map
+
+
+def _direction_90(
+    filtered_intensity: np.array, edge_map: np.array, i: int, j: int
+) -> np.array:
+    # 1. at the top edge
+    if i < 1:
+        if filtered_intensity[i, j] >= filtered_intensity[i + 1, j]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i + 1, j] = 0
+    elif i >= (edge_map.shape[0] - 1):
+        if filtered_intensity[i, j] >= filtered_intensity[i - 1, j]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i - 1, j] = 0
+    elif filtered_intensity[i, j] >= max(
+        filtered_intensity[i - 1, j], filtered_intensity[i + 1, j]
+    ):
+        edge_map[i, j] = filtered_intensity[i, j]
+        edge_map[i - 1, j] = 0
+        edge_map[i + 1, j] = 0
+    return edge_map
+
+
+def _direction_135(
+    filtered_intensity: np.array, edge_map: np.array, i: int, j: int
+) -> np.array:
+    # 1. at the left edge
+    if j < 1:
+        # 1.1 at the top left corner
+        if i == 0:
+            edge_map[i, j] = 0
+        # 1.2 at the left edge middle
+        elif filtered_intensity[i, j] >= filtered_intensity[i - 1, j + 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i - 1, j + 1] = 0
+    # 2. at the right edge
+    elif j >= (edge_map.shape[1] - 1):
+        # 1.1 at the bottom right corner
+        if i == (filtered_intensity.shape[0] - 1):
+            edge_map[i, j] = 0
+        # 1.2 at the right edge middle
+        elif filtered_intensity[i, j] >= filtered_intensity[i + 1, j - 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i + 1, j - 1] = 0
+    # 3. at the top edge
+    elif i < 1:
+        # 3.1 at the top left corner
+        if j == 0:
+            edge_map[i, j] = 0
+        # 3.2 at the top edge middle
+        elif filtered_intensity[i, j] >= filtered_intensity[i + 1, j - 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i + 1, j - 1] = 0
+    # 4. at the bottom edge
+    elif i >= (edge_map.shape[0] - 1):
+        # 4.1 at the bottom right corner
+        if j == (filtered_intensity.shape[1] - 1):
+            edge_map[i, j] = 0
+        # 4.2 at the bottom edge middle
+        elif filtered_intensity[i, j] >= filtered_intensity[i - 1, j + 1]:
+            edge_map[i, j] = filtered_intensity[i, j]
+            edge_map[i - 1, j + 1] = 0
+    # 5. middle
+    elif filtered_intensity[i, j] >= max(
+        filtered_intensity[i - 1, j + 1], filtered_intensity[i + 1, j - 1]
+    ):
+        edge_map[i, j] = filtered_intensity[i, j]
+        edge_map[i - 1, j + 1] = 0
+        edge_map[i + 1, j - 1] = 0
+    return edge_map
+
+
+def non_maximum_suppression_edge_detection(
+    sobel_x: np.array, sobel_y: np.array, threshold: float
+) -> np.array:
+    # direction map
+    direction_map = np.arctan2(sobel_y, sobel_x) * 180 / np.pi
+    direction_map = np.where(direction_map < 0, direction_map + 180, direction_map)
+    direction_map = map_to_closest_val(direction_map, np.array([0, 45, 90, 135]))
+    # calculate the intensity map
+    intensity_map = np.sqrt(sobel_x**2 + sobel_y**2)
+    intensity_filter = intensity_map > threshold
+    filtered_intensity = intensity_map * intensity_filter
+    # edge map
+    edge_map = np.zeros_like(filtered_intensity)
+    for i in range(edge_map.shape[0]):
+        for j in range(edge_map.shape[1]):
+            if direction_map[i, j] == 0:
+                edge_map = _direction_zero(filtered_intensity, edge_map, i, j)
+            elif direction_map[i, j] == 45:
+                edge_map = _direction_45(filtered_intensity, edge_map, i, j)
+            elif direction_map[i, j] == 90:
+                edge_map = _direction_90(filtered_intensity, edge_map, i, j)
+            elif direction_map[i, j] == 135:
+                edge_map = _direction_135(filtered_intensity, edge_map, i, j)
+
+    edge_map = edge_map.astype(np.uint8)
+    return edge_map
 
 
 @app.command(
@@ -119,7 +296,7 @@ def gaussian_filter_func(
     save_image(
         os.path.join(
             output_folder_path,
-            f"{input_image_path.split('/')[-1].split('.')[0]}_gaussian_{sigma}_{size}.pgm",
+            f"gaussian_{input_image_path.split('/')[-1].split('.')[0]}_{sigma}_{size}.pgm",
         ),
         result,
     )
@@ -156,6 +333,45 @@ def sobel_operator_gradient_magnitude(
         ),
         filtered_magnitude,
     )
+
+
+@app.command(
+    "non-maximum-suppression-edge-detection",
+    rich_help_panel="Edge",
+    help="Non maximum suppression edge detection",
+)
+def non_maximum_edge_detection_func(
+    input_image_path: str = typer.Option(
+        os.path.join("data/plane.pgm"), "--input", "-i", help="Input image path"
+    ),
+    output_folder_path: str = typer.Option(
+        os.path.join("result"), "--output", "-o", help="Output folder path"
+    ),
+    sigma: float = typer.Option(
+        6., "--sigma", "-s", help="Sigma (variance in gaussian filer)"
+    ),
+    gaussian_filter_size: Union[int, None] = typer.Option(
+        None,
+        "--gaussian-size",
+        "-gs",
+        help="Size of the filter, should be at least less than image size"
+        ", if it is not specified, it will be set to floor(6 * sigma + 1))",
+    ),
+    threshold: float = typer.Option(22.0, "--threshold", "-t", help="Threshold"),
+):
+    # load image
+    image = load_image(input_image_path)
+    # smooth image
+    result, size = gaussian_filter(image=image, sigma=sigma, size=gaussian_filter_size)
+    # sobel operator
+    result_x, result_y = sobel_operator(image=result)
+    # non maximum suppression
+    edge_map = non_maximum_suppression_edge_detection(
+        sobel_x=result_x, sobel_y=result_y, threshold=threshold
+    )
+    edge_map = edge_map * 225
+    img = Image.fromarray(edge_map)
+    img.save(os.path.join("result", f"edge_{input_image_path.split('/')[-1].split('.')[0]}_{sigma}_{size}_{threshold}.png"))
 
 
 @app.command(name="test-load", rich_help_panel="Test", help="Test load image")
