740e5bfc5412e0f77444406ccdb581ef4c45af0d
Haohang Li <hli113@stevens.edu>
2023-10-26 03:25:11 -0400

    fix bug

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# 1
-python run.py corner-keypoint --sigma 6.0 --threshold 16
-python run.py ransac-linefit --num-lines 4 -ns 100 -t 2
-
-# 2
-# python run.py corner-keypoint --sigma 5.0 --threshold 25
+# # key point detection
+# python run.py corner-keypoint --sigma 6.0 --threshold 16
+# # ransac line fitting
 # python run.py ransac-linefit --num-lines 4 -ns 100 -t 2
-
-# 3
-# python run.py corner-keypoint --sigma 8.0 --threshold 15
-# python run.py ransac-linefit --num-lines 4 -ns 100 -t 2
+# hough line fitting
+python run.py hough-transform --num-lines 4 --theta-step 2.0 --rho-step 2.0 --min-dist 10
+# hough line fitting half step
+# python run.py hough-transform --num-lines 4 --theta-step 1.0 --rho-step 1.0 --min-dist 10
+# hough line fitting double step
+# python run.py hough-transform --num-lines 4 --theta-step 4.0 --rho-step 4.0 --min-dist 10
